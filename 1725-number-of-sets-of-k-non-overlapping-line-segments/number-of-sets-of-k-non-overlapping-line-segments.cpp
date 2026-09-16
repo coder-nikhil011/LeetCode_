182194168e8c2eaa0de9726d316a2379dd9f96c4
Nikhil Kumar <unikhil723@gmail.com>
2026-09-17 00:04:59 +0530

    Time: 2867 ms (5.14%) | Memory: 12.4 MB (66.82%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,33 @@
+//T.C : O(n^2 * k)
+//S.C : O(n*K)
+class Solution {
+public:
+    int M = 1e9+7;
+    int t[1001][1001];
+    //O(n * k * n)
+    int solve(int n, int k, int i) {
+        if (k == 0) 
+            return 1;
+
+        if (i >= n)
+            return 0;
+
+        if(t[k][i] != -1) {
+            return t[k][i];
+        }
+
+        long long take = 0; //start segment from ith point
+        for(int j = i+1; j <= n-1; j++) {
+            take = (take + solve(n, k-1, j)) % M;
+        }
+
+        long long skip = solve(n, k, i+1) % M;
+
+        return t[k][i] = take + skip;
+    }
+
+    int numberOfSets(int n, int k) {
+        memset(t, -1, sizeof(t));
+        return solve(n, k, 0) % M;
+    }
+};
